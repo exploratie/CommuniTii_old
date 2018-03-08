d9fd038240422dc9460c887252b696aa86ed4bb2
wallexd <walee.tausif@gmail.com>
2018-03-07 22:15:34 -0500

    added nativebase

--- apps/mobile/App.js
+++ apps/mobile/App.js
@@ -1,8 +1,15 @@
 import React from "react"
+import Expo from "expo"
 
 import Screens from "./screens"
 
 export default class App extends React.Component {
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
+    })
+  }
   render() {
     return <Screens />
   }
